81850134c627c811ecc6e4fbe7118c80532f6d48
Felipe Affonso <felipe.affonso@mths.ca>
2021-04-29 03:00:25 +0000

    pizza cost final

--- pizzaCost.cpp
+++ pizzaCost.cpp
@@ -14,16 +14,19 @@ int main() {
     float cost;
     float tax;
     float total_cost;
-    
+
     std::cout << "What size of pizza do you want in inches?" << std::endl;
     std::cin >> pizza_size;
-    
+
     cost = 0.5 * pizza_size + 1.75;
-    tax = cost * 0.13;
+    tax = cost * HST;
     total_cost = tax + cost;
 
     std::cout << std::endl;
-    std::cout << "Cost: $" << std::fixed << std::setprecision(2) << std::setfill('0') << cost << std::endl;
-    std::cout << "HST: $" << std::fixed << std::setprecision(2) << std::setfill('0') << tax << std::endl;
-    std::cout << "Total Cost: $"<<  std::fixed << std::setprecision(2) << std::setfill('0') << total_cost << std::endl;
+    std::cout << "Cost: $" << std::fixed << std::setprecision(2) <<
+    std::setfill('0') << cost << std::endl;
+    std::cout << "HST: $" << std::fixed << std::setprecision(2) <<
+    std::setfill('0') << tax << std::endl;
+    std::cout << "Total Cost: $"<<  std::fixed << std::setprecision(2) <<
+    std::setfill('0') << total_cost << std::endl;
 }
